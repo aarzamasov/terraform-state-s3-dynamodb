b03f529369b34a1d9e2f744dd8e99f1296701451
Artem <Sorok7@gmail.com>
2018-08-29 17:29:38 +0200

    init

---
+++ env/main/main.tf
@@ -0,0 +1,6 @@
+module "vpc" {
+  source             = "../../modules/terraform-state-s3/"
+  s3_bucket          = "wine-tf-state2"
+  s3_bucket_description = "Second bucket for terraform"
+  dynamodb_table     = "tf-table-2"
+}
